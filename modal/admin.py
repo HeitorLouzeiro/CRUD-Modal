e6f60bda8e79638a06d0281164788cf8bb8c7346
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-01 15:28:07 -0300

    Model creation.

--- modal/admin.py
+++ modal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import exampleModal
+
+
 # Register your models here.
+class exampleModalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'fistName', 'data')
+    list_filter = ('name', 'fistName', 'data')
+    search_fields = ('name', 'fistName', 'data')
+    list_per_page = 25
+
+
+admin.site.register(exampleModal, exampleModalAdmin)
--- modal/models.py
+++ modal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class exampleModal(models.Model):
+    name = models.CharField(max_length=100)
+    fistName = models.CharField(max_length=100)
+    data = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + ' ' + self.fistName
