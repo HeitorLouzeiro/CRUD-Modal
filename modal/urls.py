2d737f918d9d2e3e455b42f5f20476216914c74b
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-01 17:55:49 -0300

    CRUD with modal.

--- modal/urls.py
+++ modal/urls.py
@@ -6,4 +6,7 @@ app_name = 'modal'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create/', views.createModal, name='createModal'),
+    path('edit/', views.editModal, name='editModal'),
+    path('delete/', views.deleteModal, name='deleteModal'),
 ]
--- modal/views.py
+++ modal/views.py
@@ -1,6 +1,57 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+from .models import exampleModal
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'modal/pages/home.html')
+    template = 'modal/pages/home.html'
+    personas = exampleModal.objects.all().order_by('name')
+    context = {'personas': personas}
+    return render(request, template, context)
+
+
+def createModal(request):
+    if not request.POST:
+        raise Http404()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        fistName = request.POST.get('fist_name')
+        exampleModal.objects.create(name=name, fistName=fistName)
+        messages.success(request, 'Data saved successfully!')
+        return redirect('modal:home')
+    else:
+        messages.error(request, 'Data not saved!')
+        return redirect(reverse('modal:home'))
+
+
+def editModal(request):
+    if not request.POST:
+        raise Http404()
+    id = request.POST.get('persona_id')
+    personaedit = get_object_or_404(exampleModal, id=id)
+    if request.method == 'POST':
+        personaedit.name = request.POST.get('name')
+        personaedit.fistName = request.POST.get('fist_name')
+        personaedit.save()
+        messages.success(request, 'Data saved successfully!')
+        return redirect('modal:home')
+    else:
+        messages.error(request, 'Data not saved!')
+        return redirect(reverse('modal:home'))
+
+
+def deleteModal(request):
+    if not request.POST:
+        raise Http404()
+
+    id = request.POST.get('persona_id')
+
+    personadelete = get_object_or_404(exampleModal, id=id)
+
+    personadelete.delete()
+    messages.success(request, 'Data successfully deleted!')
+    return redirect(reverse('modal:home'))
